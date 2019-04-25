7669ad2615180563fbb22dcb537a3c9ad9bf1d79
Hesham Gamal <38221402+Shypoup@users.noreply.github.com>
2019-04-25 16:23:16 +0200

    Project

---
+++ IA-PM/Controllers/MDController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IA_PM.Models;
+
+namespace IA_PM.Controllers
+{
+    
+    public class MDController : Controller
+    {
+        // GET: MD
+        IAEn db = new IAEn();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult add_project()
+        {
+            return View();}
+        [HttpPost]
+        public ActionResult add_project(project pro)
+        {
+            db.projects.Add(pro);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
