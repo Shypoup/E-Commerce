3a458d7039aa00bc3e438fe5f3402ebb39fb8398
Hesham Gamal <38221402+Shypoup@users.noreply.github.com>
2019-04-29 01:03:28 +0200

    home page part 1

--- IA-PM/Controllers/HomeController.cs
+++ IA-PM/Controllers/HomeController.cs
@@ -9,14 +9,43 @@ namespace IA_PM.Controllers
 {
     public class HomeController : Controller
     {
+        IAEntitiesPM db = new IAEntitiesPM();
+
         public ActionResult Index()
         {
-            return View();
+            var proj = db.projects.ToList();
+
+            homePage hp = new homePage
+            {
+                
+                projects = proj
+            };
+            return View(hp);
         }
+           
 
 
         
+       
+        public ActionResult SaveRecord(homePage proj)
+        {
+            project pro =new project();
+            pro.name = proj.project.name;
+            pro.date =Convert.ToDateTime(proj.project.date);
+            pro.descreption = proj.project.descreption;
+            pro.price = proj.project.price;
+
+            db.projects.Add(pro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
 
+        public ActionResult ListProjects()
+        {
+            var proj = db.projects.ToList();
+           
+            return View(proj);
+        }
     }
 }
---
+++ IA-PM/Models/homePage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IA_PM.Models
+{
+    public class homePage
+    {
+        public IEnumerable<project> projects { get; set; }
+        public project project { get; set; }
+    }
+}
