e45815b2864eaffcb8ed615c966619d3985c1887
Hesham Gamal <38221402+Shypoup@users.noreply.github.com>
2019-04-29 06:49:27 +0200

    MD page 1

--- IA-PM/Controllers/MDController.cs
+++ IA-PM/Controllers/MDController.cs
@@ -10,14 +10,30 @@ namespace IA_PM.Controllers
 
     public class MDController : Controller
     {
-        
+        IAEntitiesPM db = new IAEntitiesPM();
 
         public ActionResult Index()
         {
-            return View();
+
+            var md = db.users.SingleOrDefault(x => x.email == "hesham@mail.com");
+
+            var proj = db.projects.ToList();
+
+            MDPage Md = new MDPage
+            {
+                MD = md,
+
+                projects = proj
+            };
+            return View(Md);
         }
 
-      
-       
+        public ActionResult ListProjects()
+        {
+            var proj = db.projects.ToList();
+
+            return View(proj);
+        }
+
     }
 }
---
+++ IA-PM/Models/MDPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IA_PM.Models
+{
+    public class MDPage
+    {
+        public IEnumerable<project> projects { get; set; }
+        public user MD { get; set; }
+        public project project { get; set; }
+
+    }
+}
