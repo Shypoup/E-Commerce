5ae82b8b0a5b33829aa2da962e7e22ae7f7ca0b4
Hesham Gamal <38221402+Shypoup@users.noreply.github.com>
2019-04-29 23:04:32 +0200

    MT and MTL part 1

--- IA-PM/Controllers/MTController.cs
+++ IA-PM/Controllers/MTController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IA_PM.Models;
 
 namespace IA_PM.Controllers
 {
     public class MTController : Controller
     {
-        // GET: MT
+        IAEntitiesPM db = new IAEntitiesPM();
         public ActionResult Index()
         {
-            return View();
+
+            var md = db.users.SingleOrDefault(x => x.email == "hesham@mail.com");
+            var usrs = db.users.ToList();
+            var proj = db.projects.ToList();
+
+            MDPage Md = new MDPage
+            {
+                MD = md,
+                users = usrs,
+                projects = proj
+            };
+            return View(Md);
+        }
+
+        public ActionResult ListProjects()
+        {
+            var proj = db.projects.ToList();
+
+            return View(proj);
         }
     }
 }
